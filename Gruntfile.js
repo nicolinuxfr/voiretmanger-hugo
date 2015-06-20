cce8d718664fb69213e248df1df45797d0f76973
Nicolas <nicolas@mgig.fr>
2015-06-20 10:57:34 +0200

    Recherche : première version
    Utilisation de lunrjs pour une recherche plus complète.

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+var toml = require("toml");
+var S = require("string");
+
+var CONTENT_PATH_PREFIX = "content";
+
+module.exports = function(grunt) {
+
+    grunt.registerTask("lunr-index", function() {
+
+        grunt.log.writeln("Build pages index");
+
+        var indexPages = function() {
+            var pagesIndex = [];
+            grunt.file.recurse(CONTENT_PATH_PREFIX, function(abspath, rootdir, subdir, filename) {
+                grunt.verbose.writeln("Parse file:",abspath);
+                if (S(filename).endsWith(".md")) {
+					pagesIndex.push(processFile(abspath, filename));
+				}
+            });
+
+            return pagesIndex;
+        };
+
+        var processFile = function(abspath, filename) {
+            var pageIndex;
+
+            if (S(filename).endsWith(".html")) {
+                pageIndex = processHTMLFile(abspath, filename);
+            } 
+			
+			else if (S(filename).endsWith(".md")) {
+	           pageIndex = processMDFile(abspath, filename);	
+		   }
+
+            return pageIndex;
+        };
+
+        var processHTMLFile = function(abspath, filename) {
+            var content = grunt.file.read(abspath);
+            var pageName = S(filename).chompRight(".html").s;
+            var href = S(abspath)
+                .chompLeft(CONTENT_PATH_PREFIX).s;
+            return {
+                title: pageName,
+                href: href,
+            };
+        };
+
+        var processMDFile = function(abspath, filename) {
+            var content = grunt.file.read(abspath);
+            var pageIndex;
+            // First separate the Front Matter from the content and parse it
+            content = content.split("+++");
+            var frontMatter;
+            try {
+                frontMatter = toml.parse(content[1].trim());
+            } catch (e) {
+                console.failed(e.message);
+            }
+
+            var href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(".md").s;
+            // href for index.md files stops at the folder name
+            if (filename === "index.md") {
+                href = S(abspath).chompLeft(CONTENT_PATH_PREFIX).chompRight(filename).s;
+            }
+
+            // Build Lunr index for this page
+            pageIndex = {
+                title: frontMatter.title,
+                tags: frontMatter.tag,
+				acteurs: frontMatter.acteur,
+				annee: frontMatter.annee,
+				saga: frontMatter.saga,
+				original: frontMatter.original,
+                href: href,
+            };
+
+            return pageIndex;
+        };
+
+        grunt.file.write("static/PagesIndex.json", JSON.stringify(indexPages()));
+        grunt.log.ok("Index built");
+    });
+};
